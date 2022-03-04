b45047127002082fc900c28e9235f3c73d85075a
Andrew Dawson <ajdawson@acm.org>
2022-03-04 08:53:04 +0000

    Improve the program help

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,12 @@ use clap::{crate_version, Parser};
 use humantime;
 
 #[derive(Parser)]
-#[clap(version=crate_version!(), author="Andrew Dawson <andrew.dawson@ecmwf.int>")]
+#[clap(version=crate_version!())]
 #[clap(about = "Convert a time duration to a human readable form")]
 struct Opts {
-    #[clap(help = "Time duration with a unit (e.g. '352s', '652minutes')")]
+    #[clap(help = "Time duration with a unit (e.g. '352s'). \
+                   Acceptable units are: ns, us, ms, sec, min, hours, days, weeks, months, years (and few variations and abbreviations). \
+                   You can use multiple units if required (e.g. '118 minutes 227 seconds').")]
     duration: String,
 }
 
